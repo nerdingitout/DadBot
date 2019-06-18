126fdb480b057a4ffb3f78f9555afb2f21c2012d
masa <masamh@outlook.com>
2019-06-18 18:19:29 +0400

    new data files, code now randomly generates dad jokes

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var fs = require('fs');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -20,19 +22,47 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.substring(0, 2) == 'd!') {
+        var args = message.substring(2).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
             // !ping
-            case 'joke':
+            case 'joke':            
+                try {  
+                    var joke = fs.readFileSync('Joke.txt').toString().split("\n");
+                    //for(i in joke) {
+                    //    console.log(joke[i]);
+                    var jokeStr = joke[Math.floor(Math.random()*joke.length)]
+                        console.log(jokeStr);
+                    }    
+                catch(e) {
+                    console.log('Error:', e.stack);
+                }
                 bot.sendMessage({
                     to: channelID,
-                    message: 'My wife is really mad at the fact that I have no sense of direction. So I packed up my stuff and right.'
+                    message: jokeStr
                 });
             break;
+            case 'no':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'For the millionth time Karen, I told you I don\'t want salad for dinner!'
+                });
+            break;
+            case 'order':
+            bot.sendMessage({
+                to: channelID,
+                message: 'eat your brocoli, kid'
+            });
+        break;
+        case 'help':
+        bot.sendMessage({
+            to: channelID,
+            message: '*d!joke*\tfor dad joke\n*d!no*\t\tto say no like a dad\n*d!order*\tin case of the need for some parental guidance'
+        });
+        break;
             // Just add any case commands if you want to..
          }
      }
